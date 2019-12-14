785b640dbfcbcac481284d402d63713ff2f7154c
Angelo Capozzi <acap613@gmail.com>
2019-12-14 18:28:00 -0500

    starting app work, nothing connecting yet

---
+++ app/data/friends.js
@@ -0,0 +1,10 @@
+var friendArr = [
+    {
+        name: "Bugs",
+        photo: "https://cdn.vox-cdn.com/thumbor/TiwabydzgLgAVBjjJvAO_dnKo_o=/0x16:1103x751/1200x800/filters:focal(0x16:1103x751)/cdn.vox-cdn.com/uploads/chorus_image/image/46840054/Screenshot_2015-07-27_15.11.13.0.0.png",
+        scores: [ 
+
+        ]
+    },
+
+]
