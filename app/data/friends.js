7c10ebf35944b35059b1a44ba4d0da11fb4ea3b9
Angelo Capozzi <acap613@gmail.com>
2019-12-14 19:05:44 -0500

    got survey start button working

--- app/data/friends.js
+++ app/data/friends.js
@@ -2,9 +2,15 @@ var friendArr = [
     {
         name: "Bugs",
         photo: "https://cdn.vox-cdn.com/thumbor/TiwabydzgLgAVBjjJvAO_dnKo_o=/0x16:1103x751/1200x800/filters:focal(0x16:1103x751)/cdn.vox-cdn.com/uploads/chorus_image/image/46840054/Screenshot_2015-07-27_15.11.13.0.0.png",
-        scores: [ 
+        scores: [5, 3, 4, 1, 2, 1, 1, 1, 5, 3 ],
+    },
+    {
+        name: "Mario",
+        photo: "http://www8.gmanews.tv/webpics/v3/2012/03/super_mario-12816.jpg",
+        scores: [4, 5, 4, 4, 5, 5, 1, 3, 2, 5],
+    },
+    {
 
-        ]
     },
 
 ]
